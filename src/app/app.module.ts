ad11610b7892d57c035445fae20a32bb67208da9
corganfuzz <gruizcavero@uh.edu>
2018-07-04 11:41:05 -0500

    added bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/primeng';
+import {PanelModule} from 'primeng/primeng';
+import {ButtonModule} from 'primeng/primeng';
+import {RadioButtonModule} from 'primeng/primeng';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import {ScrollPanelModule } from 'primeng/scrollpanel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule,
+    RadioButtonModule,
+    ScrollPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
